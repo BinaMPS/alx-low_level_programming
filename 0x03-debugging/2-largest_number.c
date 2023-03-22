2f64b15676bc90a1177429f38c56c740d5605e53
BinaMPS <jossimoloi@gmail.com>
2023-03-22 09:12:46 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-* largest_number - returns the largest of 3 numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
-*/
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
 int largest_number(int a, int b, int c)
 {
-	int largetst;
-	
+	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -24,10 +24,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else 
+	else
 	{
 		largest = c;
 	}
 
 	return (largest);
+
 }
