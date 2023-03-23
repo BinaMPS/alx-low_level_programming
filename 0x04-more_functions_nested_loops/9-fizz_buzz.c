4417e18abd91fdf72f4c3f5812c016d2b0c38925
BinaMPS <jossimoloi@gmail.com>
2023-03-23 12:13:29 -0700

    More functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int i;
-
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
@@ -26,7 +25,7 @@ int main(void)
 			printf("%d", i);
 		} else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
